ceb5e7a354170cb55bc57d1c8119f77f29dc3280
Tomer Iwan <t.iwan@vu.nl>
2023-07-17 07:20:02 +0000

    version 0.0.2

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# vvcanvas Package
+# vvcanvas <a href="https://vusaverse.github.io/vvcanvas/"><img src="man/figures/logo.png" style="float:right; height:200px;" height="200" align="right" /></a>
+
+
+[![CodeFactor](https://www.codefactor.io/repository/github/vusaverse/vvcanvas/badge)](https://www.codefactor.io/repository/github/vusaverse/vvcanvas)
+[![R-CMD-check](https://github.com/vusaverse/vvcanvas/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/vusaverse/vvcanvas/actions/workflows/R-CMD-check.yaml)
+[![CRAN status](https://www.r-pkg.org/badges/version/vvcanvas)](https://CRAN.R-project.org/package=vvcanvas/)
+[![CRAN last month downloads](https://cranlogs.r-pkg.org/badges/last-month/vvcanvas?color=green/)](https://cran.r-project.org/package=vvcanvas/)
+[![CRAN last month downloads](https://cranlogs.r-pkg.org/badges/grand-total/vvcanvas?color=green/)](https://cran.r-project.org/package=vvcanvas/)
 
 The `vvcanvas` package is an R library that provides a convenient interface to interact with the Canvas Learning Management System (LMS) API. It allows users to authenticate, retrieve course information, fetch specific details, and perform various operations within the Canvas LMS.
 
@@ -10,7 +17,11 @@ You can install the `vvcanvas` package from GitHub using the following command:
 devtools::install_github("vusaverse/vvcanvas")
 ```
 
-The aim is to have the package hosted on CRAN in the near future.
+It is also possible to install from CRAN:
+
+```
+install.packages("vvcanvas")
+```
 
 ## Getting Started
 
---
+++ inst/doc/groups_use_case.R
@@ -0,0 +1,54 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  eval = FALSE
+)
+
+## ----setup--------------------------------------------------------------------
+#  library(vvcanvas)
+#  
+#  
+#  # Specify the Canvas API authentication details
+#  canvas <- list(
+#    api_key = "YOUR_API_KEY",
+#    base_url = "https://your_canvas_instance/"
+#  )
+#  
+#  # Retrieve all courses
+#  courses <- get_courses(canvas)
+#  
+#  # Select a course (replace the index with the desired course)
+#  course_id <- course$id[1] ## the first course in courses
+#  
+
+## ----get_groups---------------------------------------------------------------
+#  # Retrieve all groups in the selected course
+#  groups <- get_course_groups(canvas, course_id)
+#  
+#  # Extract the group IDs
+#  group_ids <- groups$id
+#  
+
+## ----get_members--------------------------------------------------------------
+#  
+#  # Retrieve group memberships using purrr
+#  memberships <- purrr::map_df(group_ids, ~{
+#    group_id <- .x
+#  
+#    # Retrieve the group's memberships
+#    group_memberships <- get_group_users(canvas, group_id)
+#  
+#    # Extract user names from memberships
+#    user_names <- group_memberships$name
+#  
+#    # Create a data frame with group memberships
+#    group_df <- data.frame(group_id = rep(group_id, length(user_names)),
+#                           user_name = user_names,
+#                           stringsAsFactors = FALSE)
+#  
+#    return(group_df)
+#  })
+#  
+#  
+
